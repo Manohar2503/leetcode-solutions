e98b50618b6503c8f5c5c387a297e894b602d111
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-07-21 13:45:58 +0530

    Time: 14 ms (99.92%), Space: 54.1 MB (56.3%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,20 +1,83 @@
 class MyHashMap {
-    private int[] arr;
+    private class Node {
+        int _key, _val;
+        Node _next;
+
+        Node(int key, int val) {
+            this(key, val, null);
+        }
+
+        Node(int key, int val, Node next) {
+            this._key = key;
+            this._val = val;
+            this._next = next;
+        }
+    }
+
+    private final int _BUCKET_SIZE = 10000;
+    private Node[] _buckets;
     public MyHashMap() {
-        arr = new int[10000001];
-        for(int i=0;i<10000001;i++) arr[i]=-1;
+        this._buckets = new Node[_BUCKET_SIZE];
     }
     
     public void put(int key, int value) {
-        arr[key] = value;
+        var index = hash(key);
+        if (this._buckets[index] == null) {
+            this._buckets[index] = new Node(key, value);
+        } else {
+            var node = this._buckets[index];
+            while (node != null) {
+                if (node._key == key) {
+                    node._val = value;
+                    return;
+                }
+
+                if (node._next == null) break;
+                node = node._next;
+            }
+
+            node._next = new Node(key, value);
+        }
+    }
+
+    private int hash(int key) {
+        return key % this._BUCKET_SIZE;
     }
     
     public int get(int key) {
-        return arr[key];
+        var index = hash(key);
+        var curr = this._buckets[index];
+        if (curr != null) {
+            while (curr != null) {
+                if (curr._key == key) {
+                    return curr._val;
+                }
+
+                curr = curr._next;
+            }
+        }
+
+        return -1;
     }
     
     public void remove(int key) {
-        arr[key] = -1;
+        var bucket = this._buckets[hash(key)];
+        if (bucket == null) {
+            return;
+        }
+
+        var prev = new Node(-1, -1, bucket);
+        var curr = prev._next;
+        while (curr != null) {
+            if (curr._key == key) {
+                prev._next = curr._next;
+                this._buckets[hash(key)] = prev;
+                return;
+            }
+
+            prev = curr;
+            curr = curr._next;
+        }
     }
 }
 
