c38ab7b93a48e64b3797765e9de3b6b565e815ca
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-07-21 11:17:42 +0530

    Time: 25 ms (78.19%), Space: 53.9 MB (60.19%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,29 +1,36 @@
-  class Heap implements Comparable<Heap>{
+  class Heap{
        int[] cord;
         double l;
         Heap(int[] cord, double l){
            this.cord = cord;
             this.l = l;
         }
-         public int compareTo(Heap a){
-                return Double.compare(this.l,a.l);
-            }
     }
 class Solution {
 
     public int[][] kClosest(int[][] points, int k) {
         List<Heap> gp = new ArrayList<>();
         for(int[] point:points){
-            int dis = point[0] * point[0] + point[1] * point[1];
+            double dis = Math.sqrt(point[1]*point[1] + point[0]*point[0]);
             gp.add(new Heap(point,dis));
         }
-        Collections.sort(gp);
-        int[][] result = new int[k][2];
-        for(int i=0;i<k;i++){
-            result[i] = gp.get(i).cord;
+        List<int[]> result = new ArrayList<>();
+        PriorityQueue<Heap> heap = new PriorityQueue<>(new Comparator<Heap>(){
+            public int compare(Heap a, Heap b){
+                return Double.compare(a.l,b.l);
+            }
+
+        });
+
+        for(Heap p : gp){
+            heap.add(p);
         }
 
-        return result;
+        while(k>0){
+            result.add(heap.poll().cord);
+            k--;
+        }
+        return result.toArray(new int[result.size()][]);
     }
 
 }
