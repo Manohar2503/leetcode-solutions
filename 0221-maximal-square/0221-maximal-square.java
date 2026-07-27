08517d1f5ed72917270316797fbb49b5dc7b24c9
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-07-27 13:14:29 +0530

    Time: 8 ms (76.79%), Space: 70.6 MB (94.96%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,49 +1,24 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int result =0;
-
-        int[] dp = new int[m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j] == '1') dp[j] +=1;
-                else dp[j] = 0; 
+        int rows = matrix.length;
+        if(rows==0) return 0;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+        int side=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]=='1'){
+                    if(i==0 || j==0){
+                        dp[i][j]=1;
+                    }else{
+                        dp[i][j] = Math.min(Math.min(dp[i-1][j],dp[i][j-1]) , dp[i-1][j-1]) +1;
+                    }  
+                }
+                side = Math.max(side,dp[i][j]);
             }
-            result = Math.max(result, helper(dp));
         }
-
-        return result;
-    }
-
-    static int helper(int[] dp){
-        int n = dp.length;
-        Stack<Integer> st = new Stack<>();
-        int result =0;
-        for(int i =0;i<n;i++){
-            int currentHeight = dp[i];
-            while(!st.isEmpty() && currentHeight < dp[st.peek()]){
-                int height = dp[st.pop()];
-                int leftBound = (st.isEmpty()?-1:st.peek());
-                int rightBound = i;
-
-                int weight = rightBound - leftBound -1;
-                int side = Math.min(weight, height);
-                result = Math.max(result, side * side);
-            }
-            st.push(i); 
-        }
-        while (!st.isEmpty()) {
-    int height = dp[st.pop()];
-    int leftBound = st.isEmpty() ? -1 : st.peek();
-    int rightBound = n;
-
-    int width = rightBound - leftBound - 1;
-    int side = Math.min(width, height);
-
-    result = Math.max(result, side * side);
-}
-
-        return result;
+        int area = side*side;
+        return area;
+        
     }
 }
