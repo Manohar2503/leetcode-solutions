18f8a2f4fe3c8b6de76feb360723d34265254158
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-08-05 15:06:56 +0530

    Time: 11 ms (4.25%), Space: 43.5 MB (6.91%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,39 +1,48 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int left=0;
-        int right=0;
-        int ans=right;
-        for(int num:nums){
-            left = Math.max(left,num);
-            right +=num;
+        int n = nums.length;
+        int totalSum =0;
+        int max = 0;
+        for(int val: nums){
+            max = Math.max(max, val);
+            totalSum += val;
         }
+        
+        int left =max;
+        int right = totalSum;
+        int result =0;
+
         while(left <= right){
-            int mid = left + (right- left )/2;
-            if(isPossible(nums,k,mid)){
-                ans=mid;
-                right=mid-1;
+            int mid = left + (right - left)/2;
+
+            if(isPossible(mid, nums, k)){
+                result = mid;
+                right = mid -1;
             }
             else{
-                left= mid+1;
+                
+                left = mid+1;
             }
         }
-        return ans;
+
+        return result;
     }
-    static boolean isPossible(int[] arr, int k, int mid){
-        int count=1;
-        int val=0;
-        for(int num:arr){
-               if(val+num <=mid){
-                val +=num;
-               }
-               else{
-               count++;
-               val=num;
-               }   
+
+    static boolean isPossible(int mid, int[] nums, int k){
+        int splits =1;
+        int currentSum =0;
+
+        for(int i=0;i<nums.length;i++){
+            if(currentSum + nums[i] <= mid){
+                currentSum = currentSum + nums[i];
+            }
+            else{
+                splits++;
+                System.out.print(splits+" ");
+                currentSum = nums[i];
+            }
         }
-        if(count<=k){
-            return true;
-        }
-        return false;
+
+        return (k>=splits)?true:false;
     }
 }
