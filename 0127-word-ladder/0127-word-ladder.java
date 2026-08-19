ad97823804aedaaf458c7d242722e49eff376bc1
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-08-19 13:21:20 +0530

    Time: 70 ms (68.46%), Space: 47.8 MB (67.33%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,36 +1,46 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         if(!wordList.contains(endWord)) return 0;
-        int res=0; 
-        Set<String> map = new HashSet<>();
-        for(int i=0;i<wordList.size();i++){
-            map.add(wordList.get(i));
+        Set<String> set = new HashSet<>();
+        for(String str: wordList){
+            set.add(str);
         }
+
+        int count =0;
         Queue<String> qu = new LinkedList<>();
         qu.offer(beginWord);
-        int m = beginWord.length();
+
         while(!qu.isEmpty()){
-            int len = qu.size();
-            res++;
-            for(int k=0;k<len;k++){
-                String word = qu.poll();
-                char[] charArr = word.toCharArray();
-                for(int i=0;i<m;i++){                
-                char ch = charArr[i];
-                for(char j='a';j<='z';++j){
-                    charArr[i]=j;
-                    String newStr = new String(charArr);
-                    if(!map.contains(newStr))
-                    continue;
-                    if(newStr.equals(endWord)) return res+1;
-                    map.remove(newStr);
-                    qu.offer(newStr);
+            int size = qu.size();
+            count++;
+            for(int i=0;i<size;i++){
+                String cur = qu.poll();
+                if(cur.equals(endWord)) return count;
+                char[] curArray = cur.toCharArray();
+                for(int j=0;j<curArray.length;j++){
+                    char c = curArray[j];
+                    for(char ch='a';ch<='z';ch++){
+                        curArray[j] = ch;
+                        String s = new String(curArray);
+                        if(set.contains(s)) {
+                            qu.offer(s);
+                            set.remove(s);
+                        }
+                    }
+                    curArray[j] = c;
                 }
-                charArr[i]=ch;
             }
-            }
+            
         }
+
         return 0;
     }
 }
 
+/*
+    hit =   ,  end = cog 
+
+    list = "hot","dot","dog","lot","log","cog"
+    
+
+*/
