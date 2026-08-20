51fe4ada50ac42d3f263268c2d45a5abb19c2eb1
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-08-20 13:19:02 +0530

    Time: 3 ms (72.84%), Space: 52.7 MB (29.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,52 @@
-class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        Map<Integer,Integer> map = new HashMap<>();
+class Solution 
+{
+    // static
+    // {
+    //     for(int i = 0; i < 500; i++)
+    //         majorityElement(new int[]{});
+    // }
+    public static List<Integer> majorityElement(int[] nums) 
+    {
+        int count1 = 0, count2 = 0;
+        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
         int n = nums.length;
-        for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            int key = entry.getKey();
-            int value = entry.getValue();
+        int min = n/3;
+        List<Integer> ans = new ArrayList<>();
 
-            if(value > (n/3)) list.add(key);
+        for(int i = 0; i < n; i++)
+        {
+            if(count1 == 0 && el2 != nums[i])
+            {
+                count1++;
+                el1 = nums[i];
+            }
+            else if(count2 == 0 && el1 != nums[i])
+            {
+                count2++;
+                el2 = nums[i];
+            }
+            else if(nums[i] == el1) count1++;
+            else if(nums[i] == el2) count2++;
+            else
+            {
+                count1--;
+                count2--;
+            }
         }
-       return list;
+
+        count1=0;
+        count2=0;
+        for(int i = 0; i < n; i++)
+        {
+            if(el1 == nums[i])
+                count1++;
+            else if(el2 == nums[i])
+                count2++;
+        }
+        if(count1 > min) ans.add(el1);       
+               
+        if(count2 > min) ans.add(el2);   
+
+        return ans;     
     }
 }
