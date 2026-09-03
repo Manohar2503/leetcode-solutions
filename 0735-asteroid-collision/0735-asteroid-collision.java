84fb428667d721bee6ce288295cc7643ed436b32
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-03 14:55:05 +0530

    Time: 2 ms (97.58%), Space: 46.8 MB (70.02%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,51 +1,34 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
         
-        for(int val: asteroids){
-            boolean isCollision = false;
-            while(!st.isEmpty() && st.peek()>0 && val < 0){
-                if(Math.abs(st.peek()) == Math.abs(val)){
-                    st.pop();
-                    isCollision = true;
-                    break;
+        int n = asteroids.length;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+
+            if(asteroids[i]>0){
+                list.add(asteroids[i]);
+            }else{
+
+                while(!list.isEmpty() && list.get(list.size()-1)>0 && Math.abs(asteroids[i])>list.get(list.size()-1)){
+                    list.remove(list.size()-1);
                 }
-                else if(Math.abs(st.peek()) > Math.abs(val)){
-                    isCollision = true;
-                    break;
-                }
-                else{
-                    st.pop();
-                }
+
+            if(!list.isEmpty() && list.get(list.size()-1)>0 && Math.abs(asteroids[i])==list.get(list.size()-1)){
+                list.remove(list.size()-1);
+            }
+            else if(list.isEmpty() || list.get(list.size()-1)<0){
+                list.add(asteroids[i]);
             }
 
-            if(!isCollision) st.push(val);
-            
+            }
+        }
+        int arr[] = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            arr[i] = list.get(i);
         }
 
-        int[] result = new int[st.size()];
-        for(int i = st.size()-1;i>=0;i--){
-            result[i] = st.pop();
-        }
+        return arr;
 
-        return result;
     }
 }
-
-/*
-
-    5 10 -5
-          i
-    st-[5, 10]
-
-    !st.isEmpty() && cur < 0 {
-        if(is top and abs(cur) is equal) // let say top = 5 , cur = -5 {
-            st.pop();
-            collison = true;
-        }
-        else if (top > cur) collison = true;
-        else{ // top < cur
-            st.pop()
-        }
-    }
-*/
