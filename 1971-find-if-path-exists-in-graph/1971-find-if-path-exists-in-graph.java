35a539115cdbeb9c5a2f46c7097270f27761e0f2
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-08 13:24:46 +0530

    Time: 13 ms (97.88%), Space: 240.9 MB (95.15%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,29 +1,27 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-        
-        for(int[] edge: edges){
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
-        }
-
-        Queue<Integer> qu = new LinkedList<>();
-        Set<Integer> set = new HashSet();
-        set.add(source);
-        qu.offer(source);
-
-        while(!qu.isEmpty()){
-            int cur = qu.poll();
-            if(cur == destination) return true;
-
-            for(int node: adj.get(cur)){
-                if(!set.contains(node)) {
-                    qu.offer(node);
-                    set.add(node);
-                }
+    class Dsu{
+        int parent[];
+        Dsu(int n){
+            parent= new int[n];
+            for(int i=0;i<n;i++){
+                parent[i]=i;
             }
         }
-        return false;
+        int find(int node){
+            if(parent[node]==node) return node;
+            return parent[node]= find(parent[node]);
+        }
+        void union(int i, int j){
+            int pi=find(i);
+            int pj=find(j);
+            parent[pi]=pj;
+        }
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        Dsu dsu= new Dsu(n);
+        for(int x[]:edges){
+            dsu.union(x[0],x[1]);
+        }
+        return dsu.find(source)==dsu.find(destination);
     }
 }
