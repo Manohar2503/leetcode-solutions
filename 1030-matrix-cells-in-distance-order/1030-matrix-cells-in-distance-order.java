efd6501f00dbefbc86c8de923e1309cffaeab34a
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-08 11:21:47 +0530

    Time: 4 ms (95.04%), Space: 48 MB (80.53%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.java
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.java
@@ -1,34 +1,33 @@
 class Solution {
-    static class Node implements Comparable<Node>{
-        int r;
-        int c;
-        int dis;
-        Node(int r, int c, int dis){
-            this.r = r;
-            this.c = c;
-            this.dis = dis;
-        }
-        
-        public int compareTo(Node other){
-            return this.dis-other.dis;
-        }
-
-    }
-    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
-        int[][] result = new int[rows * cols][2];
-        List<Node> nodes = new ArrayList<>();
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                int distance = Math.abs(i-rCenter) + Math.abs(j-cCenter);
-                nodes.add(new Node(i,j,distance));
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] result = new int[R * C][];
+        result[0] = new int[] {r0, c0};  
+        int resultIdx = 1;
+        int lim = Math.max( r0, R-r0-1 ) + Math.max( c0, C-c0-1 );
+        for (int dist = 1; dist <= lim; dist++) {
+            int r = r0 - dist;
+            int c = c0;
+            for (int count = dist; count > 0; count--) {    
+                if (r >= 0 && c >= 0)  result[resultIdx++] = new int[] {r, c};
+                r++;
+                c--;
+            }
+            for (int count = dist; count > 0; count--) {
+                if (r < R && c >= 0)  result[resultIdx++] = new int[] {r, c};
+                r++;
+                c++;
+            }
+            for (int count = dist; count > 0; count--) {
+                if (r < R && c < C)  result[resultIdx++] = new int[] {r, c};
+                r--;
+                c++;
+            }
+            for (int count = dist; count > 0; count--) {
+                if (r >= 0 && c < C)  result[resultIdx++] = new int[] {r, c};
+                r--;
+                c--;
             }
         }
-        int index =0;
-        Collections.sort(nodes);
-        for(Node n: nodes){
-            result[index++] = new int[]{n.r,n.c};
-        }
-
         return result;
     }
 }
