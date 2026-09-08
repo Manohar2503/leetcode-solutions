a5532362a11939ba732f3690c4ebf2c0e083bf0a
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-08 12:16:06 +0530

    Time: 1 ms (100%), Space: 46.2 MB (95.97%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,41 +1,21 @@
 class Solution {
+    int n,m;
     public int maxAreaOfIsland(int[][] grid) {
-        int nrows = grid.length;
-        int ncols = grid[0].length; 
-        int result =0;
-
-        for(int i=0;i<nrows;i++){
-            for(int j=0;j<ncols;j++){
-                if(grid[i][j] == 1){
-                    result = Math.max(result, bfs(i, j, grid));
-                } 
+        int count=0;
+        n=grid.length;
+        m=grid[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]>0) count =Math.max(count,DFS(i,j,grid));
             }
         }
-
-        return result;
-    }
-
-    static int bfs(int r, int c, int[][] grid){
-        Queue<int[]> qu = new LinkedList<>();
-        qu.offer(new int[]{r,c});
-        grid[r][c] =0;
-        int count =0;
-        int[][] adj = {{0,-1},{-1,0},{0,1},{1,0}};
-
-        while(!qu.isEmpty()){
-            int[] cur = qu.poll();
-            count++;
-            for(int[] n:adj){
-                int nr = cur[0] + n[0];
-                int nc = cur[1] + n[1];
-
-                if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc] ==1){
-                    grid[nr][nc] =0;
-                    qu.offer(new int[]{nr,nc});
-                }
-            }
-        }
-
         return count;
+    }
+    public int DFS(int i,int j,int[][] grid){
+        if(i<0||j<0||i>=n||j>=m||grid[i][j]<1){
+            return 0;
+        }
+        grid[i][j]=0;
+        return 1+DFS(i-1,j,grid)+DFS(i,j-1,grid)+DFS(i+1,j,grid)+DFS(i,j+1,grid);
     }
 }
