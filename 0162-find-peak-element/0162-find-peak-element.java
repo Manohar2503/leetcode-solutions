78c4416286d741332a89c720ef592431f0ba53ae
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-14 11:32:31 +0530

    Time: 0 ms (100%), Space: 43.7 MB (98.48%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,17 +1,50 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-     int n = nums.length;
-     int left =0;
-     int right = n-1;
-     while(left < right){
-        int mid = left + (right - left)/2;
-        if(nums[mid] > nums[mid+1]){    
-            right = mid;
+        int n = nums.length;
+        if(n==0) return -1;
+
+        int left =0;
+        int right = n-1;
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(mid+1 < n && nums[mid] < nums[mid+1]){
+                left = mid +1;
+            }
+            else{
+                right = mid;
+            }
         }
-        else{
-            left = mid +1;
-        }
-     }   
-     return left;
+
+        return left;
     }
 }
+
+/*
+
+1. if the array - > null  return -1
+2. the array contains duplicates 
+3. i need to return the index 
+
+
+approach:
+1. brute force : TC -> O(N), SC - > O(1)
+    i used to check each element , for each ele i check their neighbours if it is a peek ele i return immediately
+
+2. optimal solution :
+            0 1 2 3 4 5 6 - Indexs
+            1 2 1 3 5 6 4
+                      LR
+
+            mid = 5
+        1-> mid val is less then its right val -> right side
+        2-> left side
+
+        0 1 2 3
+        1 2 3 1
+        L     R
+
+        mid = 1
+        2 < 3 
+
+*/
