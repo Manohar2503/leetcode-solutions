0f9843253ef66f20b2e3c91269ffe5c0588b9bfa
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-16 15:20:50 +0530

    Time: 110 ms (69.31%), Space: 47 MB (33.35%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,29 +1,28 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+
         int n = nums1.length;
-        Map<Integer, Integer> map1 = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Store frequencies of nums1 + nums2
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int sum = nums1[i] + nums2[j];
+
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
         int result = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int val = nums1[i] + nums2[j];
-                map1.put(val, map1.getOrDefault(val,0)+1);
-            }
-        }
+        // Find the required complement
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int val = nums3[i] + nums4[j];
-                map2.put(val, map2.getOrDefault(val,0)+1);
-            }
-        }
+                int sum = nums3[i] + nums4[j];
 
-        for(Map.Entry<Integer, Integer> entry1 : map1.entrySet()){
-            for(Map.Entry<Integer, Integer> entry2 : map2.entrySet()){
-                int val1 = entry1.getKey();
-                int val2 = entry2.getKey();
-                if(val1 + val2 == 0) result += map1.get(val1) * map2.get(val2);
+                result += map.getOrDefault(-sum, 0);
             }
         }
 
