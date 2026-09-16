d63371c0adadb12cb6bf409a2550990039761986
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-16 16:17:13 +0530

    Time: 0 ms (100%), Space: 47.3 MB (97.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,35 +1,38 @@
 class Solution {
+    /*
+    water - 6 
+                  lr   
+    0 1 0 2 1 0 1 3 2 1 2 1
+
+leftmax- 3 
+rightmax-2
+    */
     public int trap(int[] height) {
-
-        Deque<Integer> stack = new ArrayDeque<>();
-
-        int water = 0;
-
-        for (int i = 0; i < height.length; i++) {
-
-            while (!stack.isEmpty() &&
-                   height[i] > height[stack.peek()]) {
-
-                int bottom = stack.pop();
-
-                if (stack.isEmpty()) {
-                    break;
+        int left =0;
+        int right = height.length-1;
+        int result =0;
+        int leftmax =0;
+        int rightmax =0;
+        while(left < right){
+            if(height[left] <= height[right]){
+                if(leftmax > height[left]){
+                    result += leftmax - height[left];
                 }
-
-                int left = stack.peek();
-
-                int width = i - left - 1;
-
-                int waterHeight =
-                    Math.min(height[left], height[i])
-                    - height[bottom];
-
-                water += width * waterHeight;
+                else{
+                    leftmax = height[left];
+                }
+                left++;
             }
-
-            stack.push(i);
+            else{
+                if(rightmax > height[right]){
+                    result += rightmax - height[right];
+                }
+                else{
+                    rightmax = height[right];
+                }
+                right--;
+            }
         }
-
-        return water;
+        return result;
     }
 }
