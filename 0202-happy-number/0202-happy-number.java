c9e08f132003f59c6a919e434231aec8b8989e98
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-17 15:41:43 +0530

    Time: 0 ms (100%), Space: 42.2 MB (88.56%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,25 +1,18 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n == 1) return true;
-        long slow = (long)n;
-        long fast = (long)n;
 
-        while((int)fast!=1){
-            slow = happy(slow); // 68
-            fast = happy(happy(fast)); // 1
-            if(fast !=1 && slow == fast) return false;
-        }
-        return true;
-    }
+        while (n != 1 && n != 4) {
+            int sum = 0;
 
-    static long happy(long n){
-        long sum = 0;
-        while(n>0){
-            long last =  n % 10;
-            sum += last * last;
-            n /= 10;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+
+            n = sum;
         }
 
-        return sum;
+        return n == 1;
     }
 }
