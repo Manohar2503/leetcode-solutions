17d72c5e4080a04e2b26368a442ea81393b9540b
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-18 15:45:05 +0530

    Time: 0 ms (100%), Space: 48.2 MB (51.56%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,26 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
+        return new int[]{binary(nums, target, true), binary(nums, target, false)};
     }
+    static int binary(int[] nums, int target, boolean isLeft){
+        int n = nums.length;
+        int left =0;
+        int right = n-1;
+        int index = -1;
 
-    private int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
-        int left = 0;
-        int right = nums.length - 1;
-        int idx = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                index = mid;
+                if(isLeft) right = mid-1;
+                else left = mid +1;
             }
+            else if(nums[mid] < target){
+                left = mid +1;
+            }
+            else right = mid -1;
         }
 
-        return idx;
+        return index;
     }
-
 }
