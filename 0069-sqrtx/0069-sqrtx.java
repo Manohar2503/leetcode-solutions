3b4b6e82252a736fa93c363735c766215bc48440
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-18 15:51:46 +0530

    Time: 1 ms (99.37%), Space: 42.7 MB (23.7%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0 || x==1) return x; 
+        long left =1;
+        long right = x;
+        long result = 0;
+        while(left<=right){
+            long mid = left + (right - left)/2;
+            long val = mid * mid;
+            if(val <= x){
+                result = mid;
+                left = mid+1;
+            }
+            else right = mid-1;
+        }   
+        return (int) result;
+    }
+}
