97f6bf4e1af1c6cdd19f2491628072e35f64cd52
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-19 13:27:09 +0530

    Time: 11 ms (63.6%), Space: 50.1 MB (75.91%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,35 +1,44 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int n = weights.length;
-        int max = 0;
-        int total =0;
-        for(int val:weights) {
-            max = Math.max(max, val);
-            total += val;
+        int left =0;
+        int right =0;
+        for(int weight: weights){
+            left = Math.max(left, weight);
+            right += weight;
         }
-        int result =0;
-        int left = max;
-        int right = total;
-        while(left <= right){
+
+        while(left < right){
             int mid = left + (right - left)/2;
-            int k =0;
+            int newDays = 0;
+
             int j=0;
-            while(j < n){
-                int sum =0;
-                while(j< n && sum+weights[j] <= mid){
-                    sum += weights[j];
+            while(j< n){
+                int count =0;
+                newDays++;
+                while(j < n && count + weights[j] <= mid) {
+                    count += weights[j];
                     j++;
                 }
-                k++;
             }
 
-            if(k <= days){
-                right = mid-1;
-                result = mid;
+            if(newDays <= days){
+                right = mid;
             }
-            else left  = mid+1;
+            else left = mid+1;
         }
 
-        return result;
+        return left;
     }
 }
+/*
+
+
+    1 2 3 4 5 6 7 8 9 10  
+
+    min = 15 
+    max = 15
+    mid = 14
+
+    ans = 15
+*/
