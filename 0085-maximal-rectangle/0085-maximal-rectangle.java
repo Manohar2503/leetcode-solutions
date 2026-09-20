240bf5229374806b58ad3a4916e31045f18f2798
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-20 15:06:16 +0530

    Time: 18 ms (76.9%), Space: 50.7 MB (45.1%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,43 +1,60 @@
-class Solution {
-    
-    public static  int maximalRectangle(char[][] matrix) {
-       
-        int n = matrix.length;
-        int m = matrix[0].length;
-        if(n==0 || m==0) return 0;
-      
-        int[] arr = new int[m];
-        int val=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]=='1'){
-                    arr[j] += 1;
-                }
-                else arr[j]=0;
-            }
-            val = Math.max(val,histogram(arr));
-        }
-        return val;
+import java.util.*;
 
-    }
-    private static int histogram(int[] arr){
-        
-        int n = arr.length;
-        Stack<Integer> st = new Stack<>();
-        int max_area = 0;
-        for(int right=0;right <=n;right++){
-            int val = (right == n) ? 0 : arr[right];
-            while(!st.isEmpty() && arr[st.peek()] > val){
-                int height = arr[st.pop()];
-                int rightboundary = right;
-                int leftboundary = (st.isEmpty())?-1:st.peek();
-                int width = rightboundary - leftboundary -1;
-                int area = height * width;
-                max_area = Math.max(max_area, area);
+class Solution {
+
+    public int maximalRectangle(char[][] matrix) {
+
+        if (matrix == null || matrix.length == 0) return 0;
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] heights = new int[cols];
+        int maxArea = 0;
+
+        for (int r = 0; r < rows; r++) {
+
+            // Build histogram heights
+            for (int c = 0; c < cols; c++) {
+                if (matrix[r][c] == '1') {
+                    heights[c] += 1;
+                } else {
+                    heights[c] = 0;
+                }
             }
-            st.push(right);
+
+            // Find largest rectangle in histogram for this row
+            maxArea = Math.max(maxArea, largestHistogram(heights));
         }
-        return max_area;
-        
+        return maxArea;
+    }
+
+
+    private int largestHistogram(int[] heights) {
+
+        int n = heights.length;
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+
+            int currHeight = (i == n) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
+
+                int height = heights[stack.pop()];
+
+                int right = i;
+                int left = stack.isEmpty() ? -1 : stack.peek();
+
+                int width = right - left - 1;
+
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
     }
 }
