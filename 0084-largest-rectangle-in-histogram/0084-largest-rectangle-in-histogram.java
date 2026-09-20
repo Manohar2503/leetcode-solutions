55e76670228717cbc056e9dff61a75608487ae11
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-20 15:05:45 +0530

    Time: 59 ms (75.99%), Space: 78.5 MB (25.93%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -8,26 +8,22 @@ class Solution {
 
         for (int i = 0; i <= n; i++) {
             int currHeight = (i == n) ? 0 : heights[i];
+
             while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
                 int height = heights[stack.pop()];
+
                 int rightBoundary = i;
                 int leftBoundary = stack.isEmpty() ? -1 : stack.peek();
+
                 int width = rightBoundary - leftBoundary - 1;
                 int area = height * width;
+
                 maxArea = Math.max(maxArea, area);
             }
+
             stack.push(i);
         }
+
         return maxArea;
     }
 }
-
-/*
-          j
-        2 1 5 6 2 3
-        0 1 2 3 4 5
-
-        peekVal > curVal 2 > 1
-        width = j - peek
-        st - [ ]
-*/
