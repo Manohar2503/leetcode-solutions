506f30ab96a4aa17107cc410bc5a3908a7f566c5
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-21 15:39:17 +0530

    Time: 0 ms (100%), Space: 46.9 MB (69.68%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -13,29 +13,14 @@
  *     }
  * }
  */
-
-
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null) return new TreeNode(val);
-        TreeNode node = new TreeNode(-1);
-       insert(root,node,val);
-       return root;
-    }
-    static void insert(TreeNode root, TreeNode prev, int val){
-        if(root == null) {
-            if(prev.val<val) {
-                TreeNode node = new TreeNode(val);
-                prev.right = node;
-            }
-            else{
-                TreeNode node = new TreeNode(val);
-                prev.left = node;
-            }
-            return;
+
+        if(root == null) return new TreeNode(val);
+        if(root.val < val){
+            root.right = insertIntoBST(root.right, val);
         }
-        
-        if(root.val<val) insert(root.right,root,val);
-        else  insert(root.left,root,val);
+        else root.left = insertIntoBST(root.left, val);
+        return root;
     }
 }
