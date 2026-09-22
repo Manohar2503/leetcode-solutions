ed774cd457f38a5c95b1a349171e0b3de8d92d4f
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-22 13:24:48 +0530

    Time: 74 ms (63.29%), Space: 47.4 MB (91.13%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,36 +1,36 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if(!wordList.contains(endWord)) return 0;
-        Set<String> set = new HashSet<>();
-        for(String str: wordList){
-            set.add(str);
-        }
+        Set<String> wordSet = new HashSet<>();
+        wordSet.addAll(wordList);
+        if(!wordSet.contains(endWord)) return 0;
 
-        int count =0;
         Queue<String> qu = new LinkedList<>();
         qu.offer(beginWord);
+        int result =0;
 
         while(!qu.isEmpty()){
             int size = qu.size();
-            count++;
-            for(int i=0;i<size;i++){
-                String cur = qu.poll();
-                if(cur.equals(endWord)) return count;
-                char[] curArray = cur.toCharArray();
-                for(int j=0;j<curArray.length;j++){
-                    char c = curArray[j];
-                    for(char ch='a';ch<='z';ch++){
-                        curArray[j] = ch;
-                        String s = new String(curArray);
-                        if(set.contains(s)) {
-                            qu.offer(s);
-                            set.remove(s);
+            result++;
+
+            for(int j=0;j<size;j++){
+                String str = qu.poll();
+                StringBuilder sb = new StringBuilder(str);
+                if(str.equals(endWord)) return result;
+
+                for(int i=0;i<str.length();i++){
+                    char ch = str.charAt(i);
+                    
+                    for(char c='a';c<='z';c++){
+                        sb.setCharAt(i,c);
+                        if(wordSet.contains(sb.toString())){
+                            qu.offer(sb.toString());
+                            wordSet.remove(sb.toString());
                         }
                     }
-                    curArray[j] = c;
+
+                    sb.setCharAt(i, ch);
                 }
             }
-            
         }
 
         return 0;
@@ -38,9 +38,13 @@ class Solution {
 }
 
 /*
-    hit =   ,  end = cog 
 
-    list = "hot","dot","dog","lot","log","cog"
-    
+    s = hit
+
+    queue - [ hit ]
+    size = 1
+
+    queue -> [ hot,]
+
 
 */
