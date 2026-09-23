bbc066e75cb0b45a0026b053f62535d0d117c59b
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-23 10:12:21 +0530

    Time: 26 ms (78.09%), Space: 53.9 MB (68.29%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -14,12 +14,12 @@ class Solution {
             double dis = Math.sqrt(point[1]*point[1] + point[0]*point[0]);
             gp.add(new Heap(point,dis));
         }
+
         List<int[]> result = new ArrayList<>();
         PriorityQueue<Heap> heap = new PriorityQueue<>(new Comparator<Heap>(){
             public int compare(Heap a, Heap b){
                 return Double.compare(a.l,b.l);
             }
-
         });
 
         for(Heap p : gp){
@@ -30,6 +30,7 @@ class Solution {
             result.add(heap.poll().cord);
             k--;
         }
+        
         return result.toArray(new int[result.size()][]);
     }
 
