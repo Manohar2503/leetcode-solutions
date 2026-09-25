9261fd4f6b128ac85fb67ae6b43c44e8a2b2e5c0
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-25 21:29:31 +0530

    Time: 2 ms (73.74%), Space: 47.1 MB (65.58%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -18,13 +18,13 @@ class Solution {
     static int path(int r, int c, int[][] matrix, int[][] dp){
         if(r<0 || r>=matrix.length || c<0 || c>=matrix.length) return Integer.MAX_VALUE;
         if(dp[r][c]!=Integer.MAX_VALUE) return dp[r][c];
-        if(r==matrix.length-1) return matrix[r][c];
+        //if(r==matrix.length-1) return matrix[r][c];
 
         int leftDown = path(r+1, c-1, matrix, dp);
         int down = path(r+1, c, matrix, dp);
         int rightDown = path(r+1, c+1, matrix, dp);
 
         int minVal = Math.min(leftDown, Math.min(rightDown, down));
-        return dp[r][c] = (minVal == Integer.MAX_VALUE)?0:minVal+ matrix[r][c];
+        return dp[r][c] = ((minVal == Integer.MAX_VALUE)?0:minVal) + matrix[r][c];
     }
 }
