e757c3440451cd939ed4ae9faf2cf1cfa211a82f
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-25 20:44:06 +0530

    Time: 1 ms (13.29%), Space: 41.9 MB (91.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -3,19 +3,12 @@ class Solution {
         int[][] dp = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j] = -1;
+                if(i==0 || j ==0) dp[i][j] = 1;
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
             }
         }
-        return paths(0,0,m,n,dp);
-    }
-    static int paths(int r, int c, int m , int n, int[][] dp){
-        if(r<0 || r>=m || c<0 || c>=n) return 0;
-        if(r==m-1 && c == n-1) return 1;
-        if(dp[r][c] !=-1) return dp[r][c];
-        int count =0;
-        count += paths(r+1, c, m, n, dp);
-        count += paths(r, c+1, m, n, dp);
-
-        return dp[r][c] = count;
+        return dp[m-1][n-1];
     }
 }
