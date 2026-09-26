331bebcc698d8e200a285ccce8f923142508fa7e
Jinka Manohar <145598597+Manohar2503@users.noreply.github.com>
2026-09-26 15:52:46 +0530

    Time: 25 ms (45.47%), Space: 49.1 MB (12.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,18 +1,36 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
-        int sum =0;
-        int result =0;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            if(map.containsKey(sum -k)){
-                result += map.get(sum -k);
-                
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        freqMap.put(0,1);
+        int prefix_Sum =0;
+        int result = 0;
+
+        for(int num: nums){
+            prefix_Sum += num;
+            if(freqMap.containsKey(prefix_Sum - k)){
+                result += freqMap.get(prefix_Sum - k);
             }
-            map.put(sum, map.getOrDefault(sum,0)+1);
+            freqMap.put(prefix_Sum, freqMap.getOrDefault(prefix_Sum, 0)+1);
         }
+
         return result;
     }
 }
+/*
+           0 1 2
+   nums =  1 1 1  , k = 2
+           i
+    prefix_sum = 3
+   
+    condition : prefix_sum - k (y) is in map  3 - 2 =1
+         count += val;// 1+1 = 2
+
+    map -> { // key : prefix_sum , val: 1
+            0 : 1
+            1 : 1,
+        }
+
+        return count;
+    TC -> O(N)
+    SC -> O(N)
+*/
